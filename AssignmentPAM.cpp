f00ad2bb72c1be6fc6893fbef1c8b8cd536a6f85
Panagiotis <scoupafi@gmail.com>
2016-11-22 00:53:51 +0200

    PAM skeleton

--- AssignmentPAM.cpp
+++ AssignmentPAM.cpp
@@ -5,11 +5,18 @@
 template <typename T>
 void PAM(Conf* myConf, Metrics* myMetric, double** distanceMatrix, ListData<T>* myList, int* centroids)
 {
+    int* new_centroids;
+    int* best_centroids;
 	cout << "IN PAM" << endl;
 	//struct with distance?
-	for (int i = 0; i < myMetric->point_number; ++i)
+	for (int i = 0; i < myConf->number_of_clusters; ++i)
 	{
 		cout << "centroid : " << i << " is " << centroids[i] << endl;
+	}
+
+	for (int i = 0; i < myMetric->point_number; ++i)
+	{
+
 	}
 
 	/*for (int i= 0; i < myMetric->point_number; ++i)
@@ -19,5 +26,5 @@ void PAM(Conf* myConf, Metrics* myMetric, double** distanceMatrix, ListData<T>* 
 			cout << "distance : " << distanceMatrix[i][centroids[i]] << endl;
 		}
 	}*/
-	
+
 }
